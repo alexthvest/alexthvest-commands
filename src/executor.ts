ab9270a48a5034183e6774b15c62359fa65063f5
alexthvest <alexthvest@gmail.com>
2019-12-25 08:56:21 +0300

    Added Middleware system

--- src/executor.ts
+++ src/executor.ts
@@ -1,6 +1,6 @@
 import { Command } from './command'
 import { Converter } from './converter'
-import { NumberConverter, StringConverter, BooleanConverter } from './converters'
+import { NumberConverter, StringConverter, BooleanConverter } from './defaults/converters'
 
 export class Executor {
   private readonly _commands: Command[]
@@ -25,21 +25,39 @@ export class Executor {
 
   public async execute(input: string, ctx: any = null) {
     const args = input.split(' ')
-    await this._executeCommand(this._commands, args, ctx)
+    await this._executeCommands(this._commands, args, ctx)
   }
 
-  private async _executeCommand(commands: Command[], args: string[], ctx: any): Promise<boolean> {
+  private async _executeCommands(commands: Command[], args: string[], ctx: any): Promise<boolean> {
+    const pattern = args[0]
+    args = args.slice(1)
+
+    const command = commands.find(c => this._isMatch(pattern, c))
+    if (!command) return false
+
+    if (!await this._executeCommands(command.commands, args, ctx)) {
+      const middleware = [...this._middleware, ctx => command.execute(ctx.args, ctx.ctx)]
+      await this._nextMiddleware(middleware, 0, { command, args, ctx })
+
+      return true
+    }
+
     return false
   }
 
-  private _nextMiddleware(index: number, ctx: MiddlewareContext) {
-    const middleware = this._middleware[index]
-    const next = this._nextMiddleware.bind(this, ++index, ctx)
+  private async _nextMiddleware(middleware: Middleware[], index: number, ctx: MiddlewareContext) {
+    const fn = middleware[index]
+    const next = this._nextMiddleware.bind(this, middleware, ++index, ctx)
 
-    if (typeof middleware !== 'function')
-      return middleware;
+    if (typeof fn === 'function')
+      await fn(ctx, next)
+  }
 
-    middleware(ctx, next)
+  private _isMatch(pattern: string, command: Command): boolean {
+    return command.patterns.some(p => {
+      if (typeof p === 'string') return pattern === p
+      return pattern.match(p)
+    })
   }
 }
 
